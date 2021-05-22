7fa4ec9c905b92f829eca41ce8890a076967f952
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-05-22 20:37:10 +0100

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+
+int main() {
+	
+
+	std::string Url = "https://www.youtube.com/watch?v=QB7ACr7pUuE";
+	
+
+
+	system(std::string("start " + Url).c_str());
+	return 0;
+}
